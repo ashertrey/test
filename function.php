1f7157b23dfaa9c6be9264d09f32dd42bf569b1f
ashertrey <79828874+ashertrey@users.noreply.github.com>
2021-05-03 15:00:27 +0200

    Update function.php

--- function.php
+++ function.php
@@ -62,3 +62,33 @@ function isLoggedIn(){
 	session_destroy();
 	unset​(​$_SESSION​[​'user'​]​)​;
 	header("location: login.php");}
+// call the login() function if register_btn is clickedif (​isset​(​$_POST​[​'login_btn'​]​)​) {
+	login();}// LOGIN USERfunction login(){
+	global $db, $username, $errors;	// grap form values	$username = e​(​$_POST​[​'username'​]​)​;
+	$password = e​(​$_POST​[​'password'​]​)​;	// make sure form is filled properly	if (empty($username)) {
+		array_push($errors, "Username is required");
+	}
+	if (empty($password)) {
+		array_push($errors, "Password is required");
+	}	// attempt login if no errors on form	if (count($errors) == 0) {
+		$password = md5($password);
+
+		$query = "SELECT * FROM users WHERE username='$username' AND password='$password' LIMIT 1";
+		$results = mysqli_query($db, $query);
+
+		if (​mysqli_num_rows​(​$results​) == 1) { // user found			// check if user is admin or user			$logged_in_user = mysqli_fetch_assoc​(​$results​)​;
+			if (​$logged_in_user​[​'user_type'​] == 'admin') {
+
+				$_SESSION['user'] = $logged_in_user;
+				$_SESSION​[​'success'​]  = "You are now logged in";
+				header('location: admin/home.php');		  
+			}else{
+				$_SESSION['user'] = $logged_in_user;
+				$_SESSION​[​'success'​]  = "You are now logged in";
+
+				header('location: index.php');
+			}
+		}else {
+			array_push($errors, "Wrong username/password combination");
+		}
+	}}
